9a789a749b11f585025c252d384695f1a65b1fef
Muhammad Ikhsan <ikhsanmhd12@gmail.com>
2022-04-20 04:27:35 +0700

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors:{
+        primary: '#3730A3',
+        dark: '#312E81',
+      },
+      screens: {
+        '2xl' : '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
